08f203e9c42bc32d46e52d5e04cc430cb5957b90
W1ndys <w1ndys@outlook.com>
2025-02-09 00:35:24 +0800

    Update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -6,8 +6,8 @@ export const nav: DefaultTheme.Config["nav"] = [
   {
     text: "示例",
     items: [
-      { text: "Markdown 示例", link: "/markdown-examples" },
-      { text: "API 示例", link: "/api-examples" },
+      { text: "Markdown 示例", link: "./markdown-examples" },
+      { text: "API 示例", link: "./api-examples" },
     ],
   },
   { text: "VitePress", link: "https://vitepress.dev/" },
